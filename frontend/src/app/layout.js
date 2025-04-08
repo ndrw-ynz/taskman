a5af2709d66e99d871dbf8a8cead70b6d5e6f68e
ndrw-ynz <andrewyanza4@gmail.com>
2025-04-08 20:38:25 +0800

    chore: cleanup layout to utilize globals.css

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,26 +1,11 @@
 "use client";
 
-import { ChakraProvider, extendTheme } from "@chakra-ui/react";
-import "@fontsource/inter";
-
-const customTheme = extendTheme({
-  fonts: {
-    heading: "Inter, sans-serif",
-    body: "Inter, sans-serif",
-  },
-  colors: {
-    primary: "#21271B",
-    secondary: "#E4FDE1",
-    background: "#515A47",
-  },
-});
+import "./globals.css";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
-        <ChakraProvider theme={customTheme}>{children}</ChakraProvider>
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
