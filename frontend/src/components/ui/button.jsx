699773c6dfb516f9b4ebc82d34aa79cd87f7a377
ndrw-ynz <andrewyanza4@gmail.com>
2025-04-18 21:56:36 +0800

    style: add hover style for rounded variant for button

--- frontend/src/components/ui/button.jsx
+++ frontend/src/components/ui/button.jsx
@@ -14,7 +14,7 @@ const buttonVariants = cva(
         navigate:
           "bg-primary text-primary-foreground shadow-xs hover:bg-primary/90 text-base rounded-full ",
         rounded:
-          "bg-primary text-primary-foreground shadow-xs hover_bg-primary/90 rounded-full",
+          "bg-primary text-primary-foreground shadow-xs hover:bg-primary/90 rounded-full",
         destructive:
           "bg-destructive text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
         outline:
