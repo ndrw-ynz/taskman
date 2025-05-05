d20d0e07a3d9b57541c7d16be9a54680093b359a
ndrw-ynz <andrewyanza4@gmail.com>
2025-05-05 20:25:57 +0800

    feat: add CustomUserDetailsService for loading user-specific data during auth process

---
+++ backend/src/main/java/com/taskman/backend/service/CustomUserDetailsService.java
@@ -0,0 +1,31 @@
+package com.taskman.backend.service;
+
+import com.taskman.backend.entity.User;
+import com.taskman.backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+/**
+ * Custom implementation of {@link UserDetailsService} for loading
+ * user-specific data during authentication process.
+ */
+@Service("CustomUserDetailsService")
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getUsername())
+                .password(user.getHashPassword())
+                .roles("USER")
+                .build();
+    }
+}
