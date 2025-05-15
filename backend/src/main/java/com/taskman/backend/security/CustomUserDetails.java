7ea294d4e85d20490624fe8369541e4ee9f484df
ndrw-ynz <andrewyanza4@gmail.com>
2025-05-15 15:56:44 +0800

    feat: add custom implementation of UserDetails with CustomUserDetails

---
+++ backend/src/main/java/com/taskman/backend/security/CustomUserDetails.java
@@ -0,0 +1,71 @@
+package com.taskman.backend.security;
+
+import com.taskman.backend.entity.User;
+import org.springframework.security.core.CredentialsContainer;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * Custom implementation of {@link UserDetails} for
+ * storing core user information.
+ */
+public class CustomUserDetails implements UserDetails, CredentialsContainer {
+
+    private Long id;
+    private String username;
+    private String password;
+
+    public CustomUserDetails(User user) {
+        this.id = user.getId();
+        this.username = user.getUsername();
+        this.password = user.getHashPassword();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+
+    @Override
+    public void eraseCredentials() {
+        this.password = null;
+    }
+}
