21bc1096a7a1db714edc36e05674efde5a3e3114
ndrw-ynz <andrewyanza4@gmail.com>
2025-05-15 16:14:24 +0800

    feat: configure stateless JWT authentication in SecurityConfig

--- backend/src/main/java/com/taskman/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/taskman/backend/config/SecurityConfig.java
@@ -1,5 +1,9 @@
 package com.taskman.backend.config;
 
+import com.taskman.backend.security.Jwt;
+import com.taskman.backend.security.JwtAuthenticationFilter;
+import com.taskman.backend.service.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,16 +13,29 @@ import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.context.NullSecurityContextRepository;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private final CustomUserDetailsService customUserDetailsService;
+
+    private final Jwt jwt;
+
+    @Autowired
+    public SecurityConfig(CustomUserDetailsService customUserDetailsService, Jwt jwt) {
+        this.customUserDetailsService = customUserDetailsService;
+        this.jwt = jwt;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -29,7 +46,10 @@ public class SecurityConfig {
                         .requestMatchers("/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST,"/taskman/api/users").permitAll()
                         .anyRequest().authenticated()
-                );
+                )
+                .sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .securityContext((context) -> context.securityContextRepository(new NullSecurityContextRepository()))
+                .addFilterBefore(new JwtAuthenticationFilter(jwt, customUserDetailsService), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
