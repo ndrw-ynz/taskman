785aca929e4834152bcfc61764099940db98fe48
ndrw-ynz <andrewyanza4@gmail.com>
2025-05-25 23:52:03 +0800

    feat: secure API endpoints for creating, updating, and deleting Lists in ListController

--- backend/src/main/java/com/taskman/backend/controller/ListController.java
+++ backend/src/main/java/com/taskman/backend/controller/ListController.java
@@ -3,10 +3,12 @@ package com.taskman.backend.controller;
 import com.taskman.backend.dto.ListCreationDTO;
 import com.taskman.backend.dto.ListResponseDTO;
 import com.taskman.backend.dto.ListUpdateDTO;
+import com.taskman.backend.security.CustomUserDetails;
 import com.taskman.backend.service.ListService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -52,11 +54,14 @@ public class ListController {
     /**
      * Creates a new list.
      *
+     * @param customUserDetails The authenticated user details from the JWT token.
      * @param listCreationDTO The details of the new list.
      * @return The details of the newly created list.
      */
     @PostMapping
-    public ResponseEntity<ListResponseDTO> createList(@RequestBody ListCreationDTO listCreationDTO) {
+    public ResponseEntity<ListResponseDTO> createList(@AuthenticationPrincipal CustomUserDetails customUserDetails, @RequestBody ListCreationDTO listCreationDTO) {
+        Long ownerId = customUserDetails.getId();
+
         ListResponseDTO createdBoard = listService.createList(listCreationDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(createdBoard);
     }
@@ -64,12 +69,13 @@ public class ListController {
     /**
      * Updates the details of the list.
      *
+     * @param customUserDetails The authenticated user details from the JWT token.
      * @param listUpdateDTO The updated details for the list.
      * @param listId The id of the list to be updated.
      * @return The details of the updated list.
      */
     @PatchMapping("/{listId}")
-    public ResponseEntity<ListResponseDTO> updateList(@RequestBody ListUpdateDTO listUpdateDTO, @PathVariable Long listId) {
+    public ResponseEntity<ListResponseDTO> updateList(@AuthenticationPrincipal CustomUserDetails customUserDetails, @RequestBody ListUpdateDTO listUpdateDTO, @PathVariable Long listId) {
         ListResponseDTO updatedBoard = listService.updateList(listUpdateDTO, listId);
         return ResponseEntity.ok(updatedBoard);
     }
@@ -77,12 +83,13 @@ public class ListController {
     /**
      * Updates the position of a list.
      *
+     * @param customUserDetails The authenticated user details from the JWT token.
      * @param listUpdateDTO The updated details for the list.
      * @param listId The id of the list to be updated.
      * @return The details of the updated list.
      */
     @PatchMapping("/position/{listId}")
-    public ResponseEntity<Void> updateListPosition(@RequestBody ListUpdateDTO listUpdateDTO, @PathVariable Long listId) {
+    public ResponseEntity<Void> updateListPosition(@AuthenticationPrincipal CustomUserDetails customUserDetails, @RequestBody ListUpdateDTO listUpdateDTO, @PathVariable Long listId) {
         listService.updateListPosition(listId, listUpdateDTO.position());
         return ResponseEntity.noContent().build();
     }
@@ -90,11 +97,12 @@ public class ListController {
     /**
      * Deletes a list by its id.
      *
+     * @param customUserDetails The authenticated user details from the JWT token.
      * @param listId The id of the list to be deleted.
      * @return {@code 204 No Content} if deletion is successful.
      */
     @DeleteMapping("/{listId}")
-    public ResponseEntity<Void> deleteBoard(@PathVariable Long listId) {
+    public ResponseEntity<Void> deleteBoard(@AuthenticationPrincipal CustomUserDetails customUserDetails, @PathVariable Long listId) {
         listService.deleteList(listId);
         return ResponseEntity.noContent().build();
     }
