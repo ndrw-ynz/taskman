bc3c008c257041b06aa4a157796838a823eb9ee8
ndrw-ynz <andrewyanza4@gmail.com>
2025-05-28 12:52:43 +0800

    feat: add CardController with API endpoints for fetching, creating, updating, and deleting Cards

---
+++ backend/src/main/java/com/taskman/backend/controller/CardController.java
@@ -0,0 +1,95 @@
+package com.taskman.backend.controller;
+
+import com.taskman.backend.dto.CardCreationDTO;
+import com.taskman.backend.dto.CardResponseDTO;
+import com.taskman.backend.dto.CardUpdateDTO;
+import com.taskman.backend.security.CustomUserDetails;
+import com.taskman.backend.service.CardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * REST controller for managing cards.
+ * Exposes endpoints for fetching, retrieving, updating, and deleting cards.
+ */
+@RestController
+@RequestMapping(path = "/taskman/api/cards")
+public class CardController {
+
+    private final CardService cardService;
+
+    @Autowired
+    public CardController(CardService cardService) {
+        this.cardService = cardService;
+    }
+
+    /**
+     * Retrieves a card by its id.
+     *
+     * @param cardId The id of the card to retrieve.
+     * @return The card if found.
+     */
+    @GetMapping("/{cardId}")
+    public ResponseEntity<CardResponseDTO> getCard(@PathVariable Long cardId) {
+        return ResponseEntity.ok(cardService.getCard(cardId));
+    }
+
+    /**
+     * Retrieves the list of all cards associated
+     * with the provided listId.
+     *
+     * @param listid The id of the list.
+     * @return The list of all cards associated with the provided listId.
+     */
+    @GetMapping("/list/{listId}")
+    public ResponseEntity<List<CardResponseDTO>> getAllCardsByListId(@PathVariable Long listid) {
+        return ResponseEntity.ok(cardService.getAllCardsByList(listid));
+    }
+
+    /**
+     * Creates a new card.
+     *
+     * @param customUserDetails The authenticated user details from the JWT token.
+     * @param cardCreationDTO The details of the new card.
+     * @return The details of the newly created card.
+     */
+    @PostMapping
+    public ResponseEntity<CardResponseDTO> createCard(@AuthenticationPrincipal CustomUserDetails customUserDetails, @RequestBody CardCreationDTO cardCreationDTO) {
+        Long ownerId = customUserDetails.getId();
+
+        CardResponseDTO createdCard = cardService.createCard(cardCreationDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdCard);
+    }
+
+    /**
+     * Updates the details of the card.
+     *
+     * @param customUserDetails The authenticated user details from the JWT token.
+     * @param cardUpdateDTO The updated details for the card.
+     * @param cardId The id of the card to be updated.
+     * @return The details of the updated card.
+     */
+    @PatchMapping("/{cardId}")
+    public ResponseEntity<CardResponseDTO> updateCard(@AuthenticationPrincipal CustomUserDetails customUserDetails, @RequestBody CardUpdateDTO cardUpdateDTO, @PathVariable Long cardId) {
+        CardResponseDTO updatedCard = cardService.updateCard(cardUpdateDTO, cardId);
+        return ResponseEntity.ok(updatedCard);
+    }
+
+    /**
+     * Deletes a card by its id.
+     *
+     * @param customUserDetails The authenticated user details from the JWT token.
+     * @param cardId The id of the card to be deleted.
+     * @return {@code 204 No Content} if deletion is successful.
+     */
+    @DeleteMapping("/{cardId}")
+    public ResponseEntity<Void> deleteCard(@AuthenticationPrincipal CustomUserDetails customUserDetails, @PathVariable Long cardId) {
+        cardService.deleteCard(cardId);
+        return ResponseEntity.noContent().build();
+    }
+}
