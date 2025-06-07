b39a99e3d8c9439ada843206eb9fc73cd6233c51
ndrw-ynz <andrewyanza4@gmail.com>
2025-06-07 15:21:17 +0800

    feat: add routing on Boards and Home buttons in left sidebar

--- frontend/src/components/general/user-left-sidebar.jsx
+++ frontend/src/components/general/user-left-sidebar.jsx
@@ -3,21 +3,24 @@ import HomeSelectionButton from "../home/home-selection-button";
 import { Activity } from "lucide-react";
 import { Separator } from "../ui/separator";
 import HomeWorkspaceDropdownMenu from "../home/home-workspace-dropdown-menu";
+import { useRouter } from "next/navigation";
 
 export default function UserLeftSidebar({ workspaces }) {
+  const router = useRouter();
+
   return (
     <div className="hidden w-[250px] flex-col space-y-2 p-4 sm:block">
-      {/* Home Selection Buttom */}
+      {/* Sidebar Selection Buttons */}
       <div className="flex flex-col space-y-1">
         <HomeSelectionButton
           icon={<TfiTrello className="h-4 w-4" />}
           name="Boards"
-          onClick={null}
+          onClick={() => router.push("/boards")}
         />
         <HomeSelectionButton
           icon={<Activity className="h-4 w-4" />}
           name="Home"
-          onClick={null}
+          onClick={() => router.push("/home")}
         />
       </div>
 
